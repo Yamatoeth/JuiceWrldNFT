1670eebd3a6b4902205aca68312ca3dd8fa1fc4e
Yamato <yamatoeth@users.noreply.github.com>
2025-08-17 22:57:08 +0700

    UI: header subtitle, contract values live, Thirdweb wallet connect, image fix

--- src/app/client.ts
+++ src/app/client.ts
@@ -2,11 +2,8 @@ import { createThirdwebClient } from "thirdweb";
 
 // Replace this with your client ID string
 // refer to https://portal.thirdweb.com/typescript/v5/client on how to get a client ID
-const clientId = process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID;
 
-if (!clientId) {
-  throw new Error("No client ID provided");
-}
+const clientId = "f2ef2c91bb3aeb736baa602bc4b4b137";
 
 export const client = createThirdwebClient({
   clientId: clientId,
